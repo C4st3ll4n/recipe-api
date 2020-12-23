404296d749fb47e6ed3d5f67c33fdac6f89faa80
Pedro Henrique <p13dr0h@gmail.com>
2020-12-23 02:06:08 -0300

    added the recipe model

---
+++ app/recipe/tests/test_recipe_api.py
@@ -0,0 +1,10 @@
+from core.models import Ingredient
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from recipe.serializers import IngredientSerializer
+from rest_framework import status
+from rest_framework.test import APIClient
+
+RECIPE_URL = reverse("recipe:recipe-list")
+
